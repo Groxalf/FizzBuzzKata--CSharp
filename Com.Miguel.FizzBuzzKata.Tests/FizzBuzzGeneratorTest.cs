45c73c2e7fc7c7521ab28cf2650e1b7ae1022890
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2015-05-25 23:23:10 +0100

    a_ten_or_more_element_string_should_contain_fizzbuzz_when_the_number_is_divisible_by_two_and_three

--- Com.Miguel.FizzBuzzKata.Tests/FizzBuzzGeneratorTest.cs
+++ Com.Miguel.FizzBuzzKata.Tests/FizzBuzzGeneratorTest.cs
@@ -7,9 +7,16 @@ namespace Com.Miguel.FizzBuzzKata.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void a_5_element_string_should_contain_fizz_buzz_when_the_number_is_divisible_by_two_or_three()
+        public void a_five_element_string_should_contain_fizz_buzz_when_the_number_is_divisible_by_two_or_three()
         {
             Assert.AreEqual("1\nFizz\nBuzz\nFizz\n5", FizzBuzzGenerator.GetChain(5));
+
+        }
+
+        [TestMethod]
+        public void a_ten_or_more_element_string_should_contain_fizzbuzz_when_the_number_is_divisible_by_two_and_three()
+        {
+            Assert.AreEqual("1\nFizz\nBuzz\nFizz\n5\nFizzBuzz\n7\nFizz\nBuzz\nFizz", FizzBuzzGenerator.GetChain(10));
 
         }
     }
